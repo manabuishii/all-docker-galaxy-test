ea647511cfce79298669029d5d907953f93ef6f8
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-03-02 22:00:04 +0900

    Scripts to test

---
+++ exec.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for VERSION in dev latest 15.03 15.05 15.07 15.10 16.01 16.04 16.07 16.10 17.01
+#for VERSION in dev latest 
+do
+  echo 
+  echo "Version[${VERSION}]"
+  docker run --rm bgruening/galaxy-stable:${VERSION} $@
+done
+
